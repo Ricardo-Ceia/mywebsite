33affa8124a6adcdaf9c0441b7a2e65476569787
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-11-19 23:07:09 +0000

    add initial animation and better the projects cards

--- main.go
+++ main.go
@@ -3,17 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 
-	http.Handle("/",fileServer)
+	http.Handle("/", fileServer)
+
+	server := &http.Server{
+		Addr:         ":8000",
+		Handler:      nil, // Uses DefaultServeMux
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
 
 	log.Println("port running on http://localhost:8000")
-	if err:=http.ListenAndServe(":8000",nil);err!=nil{
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
-	
-}
 
+}
